9c6bd8f84b6a0eca9ff8893be77d052b1cf413ab
ktelesh@gmail.com <ktelesh@gmail.com>
2020-05-19 16:30:53 +0200

    first commit

---
+++ baby_object.py
@@ -0,0 +1,45 @@
+class BoboChoses(object):
+    dresses = '[class="campaign has-text  text-plate c-4 "]:nth-of-type(4n)'
+    article = 'article:nth-of-type(1n)'
+    quickshop = 'div.quickshop-button '
+    drop_down = '#id-slct'
+    option = '#id-slct ul li:nth-of-type(5n)'
+    add_cart = 'button[class="add-to-cart green large"]'
+    p_name = 'p.name'
+    checkout = 'a[class="to-checkout white button"]'
+    delete = 'a.remove'
+    msg = 'Your cart is empty'
+    DG = 'a[title="Dolce & Gabbana"]'
+    filter_by = '.filter-label'
+    price = 'span.lowest'
+    region_change = 'a[href="/country-and-language"]'
+    ru_url = 'https://ru.babyshop.com/country-and-language'
+    brands = 'a[data-class="brand"]'
+    postal_code = 'input[name="postalCode"]'
+    city = 'input[name="city"]'
+    phone = 'input[name="phoneNumber"]'
+    adress = 'input[name="addressLine2"]'
+    firstName = 'input[name="firstName"]'
+    lastName = 'input[name="lastName"]'
+    email = 'input[name="emailAddress"]'
+    submit = 'button[type="submit"]'
+
+
+
+
+
+
+
+
+
+    
+    # search_box = 'input[title="Search"]'
+    # list_box = '[role="listbox"]'
+    # search_button = 'input[value="Google Search"]'
+    # feeling_lucky_button = '''input[value="I'm Feeling Lucky"]'''
+
+
+# class ResultsPage(object):
+#     google_logo = 'img[alt="Google"]'
+#     images_link = 'link=Images'
+#     search_results = 'div#center_col'
---
+++ test_babyshop.py
@@ -0,0 +1,78 @@
+from seleniumbase import BaseCase
+from .baby_object import BoboChoses
+
+class NavigationTest(BaseCase):
+
+    def setUp(self):
+        super(NavigationTest, self).setUp()
+        self.base_url = 'https://www.babyshop.com'
+        self.menu_dict = {'Brands':['//a[@data-class="brand"]',self.base_url+'/brands/s/618']}
+
+    def test_menu(self):
+        # Откроем сайт
+        self.get(self.base_url)
+        for item in self.menu_dict:
+            print(f'Menu {item}')
+            self.click(self.menu_dict[item][0])
+            # получаем текущий урл в переменную curr_url
+            curr_url = self.get_current_url()
+            # проверка соответствия полученного и ожидаемого урл
+            self.assert_true(curr_url == self.menu_dict[item][1])
+
+    def test_search(self):
+        self.get(self.base_url)
+        self.update_text('input[name="key"]', "Sirona S i-Size Car Seat\n")
+        number = len(self.find_elements('article'))
+        self.assert_equal(number, 5)
+
+    def test_basket(self):
+        self.open(self.base_url+'/bobo-choses/s/466')
+        self.click(BoboChoses.dresses)
+        self.hover_and_click(BoboChoses.article, BoboChoses.quickshop)
+        self.click(BoboChoses.drop_down)
+        self.click(BoboChoses.option)
+        self.click(BoboChoses.add_cart)
+        name = self.get_text(BoboChoses.p_name)[:26]
+        self.click(BoboChoses.checkout)
+        self.assert_text_visible(name)
+        self.click(BoboChoses.delete)
+        self.assert_text_visible(BoboChoses.msg)
+        self.sleep(1)
+
+    def test_lang(self):
+        self.open(self.menu_dict['Brands'][1])
+        self.click(BoboChoses.DG)
+        eng = self.get_text(BoboChoses.filter_by)
+        price = self.get_text(BoboChoses.price)
+        self.click(BoboChoses.region_change)
+        self.click_link_text('Русский')
+        url = self.get_current_url()
+        # Check url change
+        self.assertEqual(url, BoboChoses.ru_url)
+        self.click(BoboChoses.brands)
+        self.click(BoboChoses.DG)
+        ru = self.get_text(BoboChoses.filter_by)
+        ru_price = self.get_text(BoboChoses.price)
+        # Check site language changed
+        self.assertNotEqual(eng, ru)
+        # Check price (currency and number) changed
+        self.assertNotEqual(price, ru_price)
+        self.sleep(2)
+
+    def test_buy(self):
+        self.open(self.base_url+'/bobo-choses/s/466')
+        self.click(BoboChoses.dresses)
+        self.hover_and_click(BoboChoses.article, BoboChoses.quickshop)
+        self.click(BoboChoses.drop_down)
+        self.click(BoboChoses.option)
+        self.click(BoboChoses.add_cart)
+        self.click(BoboChoses.checkout)
+        self.update_text(BoboChoses.email, 'b9euqngtxy@1secmail.net')
+        self.update_text(BoboChoses.firstName, 'George')
+        self.update_text(BoboChoses.lastName, 'Call')
+        self.update_text(BoboChoses.adress, 'Daalmeereiland 40')
+        self.update_text(BoboChoses.postal_code, '1827KX')
+        self.update_text(BoboChoses.city, 'Alkmaar')
+        self.update_text(BoboChoses.phone, '0629564634')
+        # self.click(BoboChoses.submit)
+        self.sleep(5)
